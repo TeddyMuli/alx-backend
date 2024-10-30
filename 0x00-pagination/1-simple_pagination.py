47051dcd52431931414febdca3811124bb0cd87e
TeddyMuli <tmuli974@gmail.com>
2024-10-30 10:11:48 +0300

    fix pylint errors

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+"""
+Module for simple pagination.
+"""
 import csv
 from typing import List
-"""Module for simple pagination
-"""
 
 
 index_range = __import__('0-simple_helper_function').index_range
@@ -19,14 +20,17 @@ class Server:
         """Cached dataset
         """
         if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
+            with open(self.DATA_FILE, encoding='utf-8') as file:
+                reader = csv.reader(file)
+                dataset = list(reader)
             self.__dataset = dataset[1:]
 
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Get a page from the dataset.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
