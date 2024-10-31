491345d33d24d7a29838c1c141b936654ae681fc
TeddyMuli <tmuli974@gmail.com>
2024-10-31 23:12:28 +0300

    add 0-basic_cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+""" Create a class BasicCache that inherits from BaseCaching and is a caching system:
+    You must use self.cache_data - dictionary from the parent class BaseCaching
+    This caching system doesn't have limit
+    def put(self, key, item):
+    Must assign to the dictionary self.cache_data the item value for the key key.
+    If key or item is None, this method should not do anything.
+    def get(self, key):
+    Must return the value in self.cache_data linked to key.
+    If key is None or if the key doesn’t exist in self.cache_data, return None.
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """ Add an item to cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item from cache by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
