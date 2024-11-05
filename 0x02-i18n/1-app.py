1b0ed0212ce11f8524bc8e87acf3916dbb61f304
TeddyMuli <tmuli974@gmail.com>
2024-11-05 18:24:42 +0300

    initialize babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+from flask import Flask
+from flask_babel import Babel
+
+
+class Config:
+    """ Configuration for babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+babel = Babel(app)
