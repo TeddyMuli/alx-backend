9ca8a91247ade839a215bdb770fce7acb69e0897
TeddyMuli <tmuli974@gmail.com>
2024-12-04 16:23:53 +0300

    task 0

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,9 +1,7 @@
-import { createClient } from "redis";
+import { createClient } from 'redis';
 
 const client = createClient();
 client.on('error', (err) => console.error('Redis client not connected to the server: ', err));
 
 await client.connect();
 console.log('Redis client connected to server');
-
-await client.quit();
