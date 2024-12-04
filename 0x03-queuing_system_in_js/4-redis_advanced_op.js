8c44a660508a2f87217d4378480ad620d8786515
TeddyMuli <tmuli974@gmail.com>
2024-12-04 17:10:13 +0300

    finish task 3

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,35 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+const updateHash = (hashName, fieldName, fieldValue) => {
+  client.hSet(hashName, fieldName, fieldValue);
+};
+
+const printHash = (hashName) => {
+  client.hGetAll(hashName, (_err, reply) => console.log(reply));
+};
+
+function main() {
+  const hashObj = {
+    Portland: 50,
+    Seattle: 80,
+    'New York': 20,
+    Bogota: 20,
+    Cali: 40,
+    Paris: 2,
+  };
+  for (const [field, value] of Object.entries(hashObj)) {
+    updateHash('HolbertonSchools', field, value);
+  }
+  printHash('HolbertonSchools');
+}
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+  main();
+});
